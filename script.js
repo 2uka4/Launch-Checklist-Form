79c6db5307f5ba8cac54a062dca587ad22bfc2b6
2uka4 <uuganchimeg1104@gmail.com>
2021-06-18 19:02:23 -0500

    latest

--- script.js
+++ script.js
@@ -1,73 +1,54 @@
-// Write your JavaScript code here!
-
-// window.addEventListener("load", function() {
-//    let form = document.querySelector("form")
-//    form.addEventListener("submit", function(event) {
-//       let pilotName = document.querySelector("input[name=pilotName]");
-//       let copilotName = document.querySelector("input[name=copilotName]");
-//       let fuelLevel = document.querySelector("input[name=fuelLevel]");
-//       let cargoMass = document.querySelector("input[name=cargoMass]");
-
-//       if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "")  {
-
-//          alert("All fields are required!");
-//          event.preventDefault();
-//       }
-//       if ((typeof pilotName.value !== "string") || (typeof copilotName.value !== "string" || (typeof Number(fuelLevel.value) !== "number")))  {
-//          alert("Appropriate data type is required");
-//          // stop the form submission
-//          event.preventDefault();
-//       }
-
-//    let list = document.getElementById("faultyItems")
-//    list.style.visibility = "hidden"
 
 window.addEventListener("load", function() {
    // do the fetch json here
-   });
+   
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
-      event.preventDefault();
+      // event.preventDefault();
       let pilotName = document.querySelector("input[name=pilotName]");
       let copilotName = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
    
-      pilotStatus = document.getElementById("pilotStatus");
-      copilotStatus = document.getElementById("copilotStatus");
-      fuelStatus = document.getElementById("fuelStatus");
-      cargoStatus = document.getElementById("cargoStatus");
-      let launchStatus = document.getElementById("launchStatusCheck");
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+      let launchStatus = document.getElementById("launchStatus");
       let faultyItems = document.getElementById("faultyItems");
+      faultyItems.style.visibility = "hidden";
       //put logic here to for valid input
+
    if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "")  {
 
       alert("All fields are required!");
+      event.preventDefault();
    }
 
    if ((typeof pilotName.value !== "string") || (typeof copilotName.value !== "string" || (typeof Number(fuelLevel.value) !== "number") || (typeof Number(cargoMass.value !== "number")))){
-      alert("Appropriate data type is required");
+      // alert("Appropriate data type is required");
+      event.preventDefault();
    }
-   pilotStatus = document.getElementById("pilotStatus").innerHTML = `${pilotStatus} is ready`;
-   copilotStatus = document.getElementById("copilotStatus").innerHTML = `${copilotStatus} is ready`;
 
       //Put logic here to display pilot name and check for fuel level and mass input
+
    if ((fuelLevel.value) <= 10000) {
       let faultyItems = document.getElementById("faultyItems")
       faultyItems.style.visibility = "visible"
+      // pilotStatus = document.getElementById("pilotStatus").innerHTML = `${pilotName} 'is ready`;
+      // copilotStatus = document.getElementById("copilotStatus").innerHTML = `${copilotName} is ready`;
    }
    if ((cargoMass.value) >= 10000){
-
+      let launchStatus = document.getElementById("launchStatus")
+      launchStatus.innerHTML = "Shuttle not ready for launch "
+      launchStatus.style.color = "red"
+   } else{ 
+      launchStatus.style.color = "green"
+      launchStatus.innerHTML = "Shuttle is ready for launch "
+      faultyItems.style.visibility = "visible"
    }
 })
-
-
-
-
-
-
-
-
+});
 
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
